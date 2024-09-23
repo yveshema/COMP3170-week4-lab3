25d512648ff9132133d12b7acc25a8e0403e320d
yves shema <yshema@bcit.ca>
2024-09-23 10:09:09 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import news_feed from './sample_news_stories.json';
+
+function App() {
+
+  // Get stories from news feed
+  const stories = news_feed.results;
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
